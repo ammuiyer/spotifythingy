193cbc137270b435e49a15239f6b305e6afcad9f
ammuiyer <ammu.iyer5@gmail.com>
2024-02-17 00:40:20 -0500

    yay

--- main.py
+++ main.py
@@ -12,18 +12,8 @@ os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
 redirect = os.getenv("REDIRECT_URI")
-# token_url = "https://accounts.spotify.com/api/token"
-# spotify = OAuth2Session(client_id=client_id, scope=["playlist-modify-private", "playlist-modify-public"], redirect_uri=redirect)
-# print('Please go here and authorize: ', spotify.authorization_url("https://accounts.spotify.com/authorize"))
-
-
-# redirect_response = input('\n\nPaste the full redirect URL here: ')
-
-# from requests.auth import HTTPBasicAuth
-
-# auth = HTTPBasicAuth(client_id, client_secret)
-
-# Fetch the access token
+playlist_name = os.getenv("PLAYLIST_NAME")
+username = os.getenv("USERNAME")
 
 
 def get_token():
@@ -63,41 +53,26 @@ def search_for_track(token, keyword):
     if len(json_result) == 0:
         print("No tracks exist under this artist!")
         return None
-    return json_result[0]
+    return json_result[0]['uri']
 
     # use the get method for this endpoint
 
-def create_playlist(user_id):
-    url = 'https://api.spotify.com/v1/users/' + user_id +'/playlists'
-
-    token = spotify.fetch_token(token_url, auth=auth, authorization_response=redirect_response)
-
-    data = {
-    "name": "New Test Playlist"}
-
-    #make a post
-    result = spotify.post(url, data=data, json=True)
-    print(result.content)
-    json_result = json.loads(result.content)
-    return json_result
    
 
-def request_token():
+def request_token_auth_flow():
 
-    #  1. Your application requests authorization
+
     auth_url = 'https://accounts.spotify.com/authorize'
-    payload = {'client_id': client_id, 'response_type':'code','redirect_uri':redirect}
+    payload = {'client_id': client_id, 'response_type':'code','redirect_uri':redirect, 'scope': "playlist-modify-private  playlist-modify-public playlist-read-private"}
     auth = requests.get(auth_url,params = payload)
     print ('\nPlease go to this url to authorize ', auth.url)
 
-    #  2. The user is asked to authorize access within the scopes
-    #  3. The user is redirected back to your specified URI
     resp_url = input('\nThen please copy-paste the url you where redirected to: ')
     resp_code= resp_url.split("?code=")[1].split("&")[0]
 
-    #  4. Your application requests refresh and access tokens
+
     token_url = 'https://accounts.spotify.com/api/token'
-    payload = {'redirect_uri': redirect,'code': resp_code, 'grant_type': 'authorization_code','scope':'playlist-modify-private playlist-modify-public playlist-read-private'}
+    payload = {'redirect_uri': redirect,'code': resp_code, 'grant_type': 'authorization_code'}
     auth_string = client_id + ":" + client_secret
     auth_bytes = auth_string.encode("utf-8")
     auth_enconded = str(base64.b64encode(auth_bytes), "utf-8")
@@ -107,21 +82,35 @@ def request_token():
 
     return response
    
-# token = get_token()
-# print(search_for_track(token, "chase atlantic")["name"])
-# print(create_playlist('ammuiyer'))
 
-def create_playlist(username, list_name):
-    token = request_token()
-    access_token = token['access_token']
-    auth_header = {'Authorization': 'Bearer {token}'.format(token=access_token), 'Content-Type': 'application/json'}
-    api_url = 'https://api.spotify.com/v1/users/%s/playlists' % username
+#returns spotify id of da playlist
+def create_playlist(username, token):
+    auth_header = get_auth_header(token)
+    api_url = 'https://api.spotify.com/v1/users/' + username + '/playlists'
     data = {
-    "name": "New Test Playlist"}
+    "name": playlist_name}
     r = requests.post(api_url, data=json.dumps(data), headers=auth_header)
-    print(r.content)
+    response = r.json()
+    return response["id"]
 
-print(create_playlist('ammuiyer', "new playlist"))
+def add_song_to_playlist(playlist_id, songs_list=None):
+    auth_header = get_auth_header(token)
+    api_url = 'https://api.spotify.com/v1/playlists/' + playlist_id + '/tracks'
+    data = {
+    "playlist_id": playlist_id
+    }
+    r = requests.post(api_url, data=json.dumps(data), headers=auth_header)
+    response = r.json()
+    return response["id"]
+
+token = get_token()
+print(search_for_track(token, "chase atlantic"))
+
+token = request_token_auth_flow()['access_token']
+playlist_id = create_playlist(username=username, token=token)
+add_song_to_playlist(playlist_id)
+
+
 
 
 
